3b5c50267164780b9dce67bcbeb9c2bf6f271fef
William Cusick <17467136+wrcusic@users.noreply.github.com>
2025-05-21 07:01:45 -0600

    Update passwordChange.ps1

--- Scripting/PowerShell/passwordChange.ps1
+++ Scripting/PowerShell/passwordChange.ps1
@@ -1,6 +1,11 @@
-# Automation script to reset passwords while in user-land
+# Automation script to reset local user passwords
+
+ECHO 'Y' Set-ExecutionPolicy Bypass
 
 $Password = ConvertTo-SecureString -String "[enterPassword]" -AsPlainText -Force
 $Username = Get-LocalUser -Name "[username]"
 
 $Username | Set-LocalUser -Password $Password
+
+# Uncomment if you want the user's password to never expire
+#$Username | Set-LocalUser -PasswordNeverExpires $True
