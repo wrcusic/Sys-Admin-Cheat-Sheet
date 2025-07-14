46403c3c11aa34346c4a20b888e91684f970d87f
William Cusick <17467136+wrcusic@users.noreply.github.com>
2025-07-14 16:13:21 -0600

    Update setImmutID.ps1

--- Powershell/Entra Sync/setImmutID.ps1
+++ Powershell/Entra Sync/setImmutID.ps1
@@ -1,3 +1,3 @@
 #Bulk update ImmutableID based on a CSV file
-$var = Import-CSV C:\Users\Spectrumadmin\Desktop\immutableID.csv
+$var = Import-CSV path\to\file\immutableID.csv
 $var | % { Update-MgUser -UserId $_.UserPrincipalName -OnPremisesImmutableId $_.ImmutableId }
